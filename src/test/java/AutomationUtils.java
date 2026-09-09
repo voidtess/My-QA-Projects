3c98e355b723bc1028a1b66e3970a1c98483d854
voidtess <voidtwone@gmail.com>
2026-09-09 11:06:11 +0300

    Patch: update TestUtils name in all classes

--- src/test/java/AutomationUtils.java
+++ src/test/java/AutomationUtils.java
@@ -1,14 +1,13 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.time.Duration;
 
-public class TestUtils {
+public class AutomationUtils {
     // This is a utility class for common test operations, starting a WebDriver, clicking links, waiting for elements
     public static WebDriver start(String relativeHtmlPath) {
         WebDriver driver = new ChromeDriver();
